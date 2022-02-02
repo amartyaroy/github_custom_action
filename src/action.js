7e1ee1cac626066ce1dc26c25400a6b522209616
amartyaroy <amartyaroykumar001@gmail.com>
2022-02-02 20:09:44 +0530

    Say hello to new pull request

--- src/action.js
+++ src/action.js
@@ -1,4 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
 async function run(){
+    const githubToken = core.getInput('GITHUB_TOKEN')
+
+    const ocktokit = github.getOctokit(githubToken)
+
+    const { context = {} } = github
+    const {pull_request} = context.payload
+
+    await octokit.rest.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body:'Thank you for submitting a pull request!We will try to review it ASAP',
+      });
+
     console.log("Hello world")
 }
 
